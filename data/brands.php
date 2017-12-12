526953e076975012e16505e81579735da856fd5e
Suhajda Peter <p.suhajda@marquardmedia.hu>
2017-12-12 11:48:37 +0100

    Brand, product and depot config files

---
+++ data/brands.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'name'       => 'Samsung',
+        'quality'    => 5,
+    ],
+    [
+        'name'       => 'Apple',
+        'quality'    => 5,
+    ],
+    [
+        'name'       => 'Huawei',
+        'quality'    => 4,
+    ],
+];
---
+++ data/depots.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    [
+        'name'        => 'Primary depot',
+        'capacity'    => 100,
+        'address'     => '112244 Tápiószecső, Macskaköves út 0.',
+        'products'    => [
+            [
+                'name'       => 'Samsung Galaxy S8',
+                'quantity'   => 30,
+            ],
+            [
+                'name'       => 'Samsung GearVR 2',
+                'quantity'   => 40,
+            ],
+            [
+                'name'       => 'Apple iPhone X',
+                'quantity'   => 20,
+            ],
+        ],
+    ],
+    [
+        'name'        => 'Secondary depot',
+        'capacity'    => 1000,
+        'address'     => '442211 Angyalbögyörővalagpuszta, Sugár utak 999.',
+        'products'    => [
+            [
+                'name'       => 'Samsung Galaxy S8',
+                'quantity'   => 100,
+            ],
+            [
+                'name'       => 'Samsung GearVR 2',
+                'quantity'   => 326,
+            ],
+            [
+                'name'       => 'Apple iPhone X',
+                'quantity'   => 41,
+            ],
+            [
+                'name'       => 'Huawei Mate 10 Pro',
+                'quantity'   => 127,
+            ],
+        ],
+    ],
+];
